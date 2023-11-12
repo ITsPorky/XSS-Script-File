36fa57172202b23ed3c99a3209d9c33d7b1a1da1
Wade <wc.walsh16@gmail.com>
2023-11-12 10:10:55 +1000

    Update xss-script.js

--- xss-script.js
+++ xss-script.js
@@ -1,1 +1,10 @@
-f
+// self executing function here
+(function() {
+   // your page initialization code here
+   // the DOM will be available here
+  callAlert("XSS is possible...");
+})();
+
+function callAlert(text) {
+  window.alert(text);
+};
